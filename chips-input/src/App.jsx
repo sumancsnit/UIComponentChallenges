681996aa7d3d59df3e5962c2218764e1d9925cb5
suman kumar <sumancsnit@gmail.com>
2025-04-29 19:09:02 +0530

    chips input

---
+++ chips-input/src/App.jsx
@@ -0,0 +1,12 @@
+import ChipsInput from './ChipsInput';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <ChipsInput />
+    </>
+  );
+}
+
+export default App;
---
+++ chips-input/src/ChipsInput.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const ChipsInput = () => {
+  const [value, setValue] = useState('');
+  const [chips, setChips] = useState([]);
+  const [idCounter, setIdCounter] = useState(0);
+
+  const handleInputChange = (event) => {
+    const { value } = event.target;
+    setValue(value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && value.trim() !== '') {
+      const newChip = {
+        id: idCounter,
+        label: value.trim(),
+      };
+      setChips([...chips, newChip]);
+      setIdCounter((idCounter) => idCounter + 1);
+      setValue('');
+    }
+  };
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        margin: '30px 0',
+      }}
+    >
+      <h2>Chips Input</h2>
+      <input
+        type='text'
+        value={value}
+        onChange={handleInputChange}
+        onKeyDown={handleKeyPress}
+        placeholder='Type a chip name...'
+        style={{ padding: '0.5rem', width: '200px' }}
+      />
+      <div
+        style={{
+          margin: '1rem',
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+        }}
+      >
+        {chips.map((chip) => (
+          <div
+            key={chip.id}
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              margin: '0.5rem',
+              backgroundColor: 'lightskyblue',
+              borderRadius: '1rem',
+              padding: '5px 10px',
+            }}
+          >
+            <span>{chip.label}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ChipsInput;
